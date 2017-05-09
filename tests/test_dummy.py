4b3ce05cc5772e827b87ab071b1c1684abb80d55
Fapfood <fapfood@gmail.com>
2017-05-09 18:21:28 +0200

    tests

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -16,6 +16,26 @@ class TestDummy(unittest.TestCase):
         retcode = main(("--year", "2001", "--month", "1", "--day", "3"))
         self.assertEqual(retcode, 0)
 
+    def test_correct_day(self):
+        weekday = calculate(2017, 5, 9)
+        self.assertEqual(weekday, 1)
+
+        retcode = main(("--year", "2017", "--month", "5", "--day", "9"))
+        self.assertEqual(retcode, 1)
+
+    def test_string_instead_int(self):
+        calculate('2017', '5', '9')
+        self.assertRaises(TypeError)
+
+    def test_too_big_number_of_day(self):
+        calculate(2017, 5, 35)
+        self.assertRaises(BaseException)
+
+        main(("--year", "2017", "--month", "5", "--day", "35"))
+        self.assertRaises(BaseException)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
